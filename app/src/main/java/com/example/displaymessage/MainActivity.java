73234b66196a0ec70f6fd457adf33523a46848bc
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-03-27 16:51:28 +0530

    Display Message

--- app/src/main/java/com/example/displaymessage/MainActivity.java
+++ app/src/main/java/com/example/displaymessage/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String EXTRA_MESSAGE = "com.example.displaymessage.Message";
     EditText msg1;
     Button send;
 
@@ -16,13 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        msg1= findViewById(R.id.ET1_main);
+
+        msg1 = findViewById(R.id.ET1_main);
         send = findViewById(R.id.btn_main);
-        
-        Intent getMessage = new Intent(MainActivity.this,DisplayActivity.class);
-        String name = msg1.getText().toString();
-        getMessage.putExtra("TextMessage",name);
-        startActivity(getMessage);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendMessage = new Intent(MainActivity.this, DisplayActivity.class);
+                String message = msg1.getText().toString();
+
+                sendMessage.putExtra(EXTRA_MESSAGE,message);
+                startActivity(sendMessage);
+            }
+        });
 
     }
 }
